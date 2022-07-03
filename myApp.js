6a4c648709cdb1f3806ad1981c5271174203d80c
Jim Lam <jimlam@146-109-254-113-on-nets.com>
2022-07-03 17:10:43 +0800

    serving Jam

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 let express = require('express');
 let app = express();
 let spotlight = __dirname + "/views/index.html";
+let stylist = __dirname + "/public/style.css";
+let square = __dirname + "/public";
 console.log("Hello World");
+app.use(
+  square,
+  express.static(stylist)
+);
 app.get('/', function(req, res) {
     res.sendFile(spotlight);
   });
