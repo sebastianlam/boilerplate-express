e5c6d1d4d97fdbce516bdb0352d64110ab948d72
Jim Lam <jimlam@146-109-254-113-on-nets.com>
2022-07-04 18:08:03 +0800

    serving case changes

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
+let envConfig = require('dotenv').config();
 let express = require('express');
 let app = express();
 let spotlight = __dirname + "/views/index.html";
 let public = __dirname + "/public";
-console.log("Hello World");
+console.log("Hello World", envConfig);
+let defMessage = {"message": "Hello json"};
 app.use(
   "/public",
   express.static(public)
@@ -11,6 +13,9 @@ app.get('/', function(req, res) {
   res.sendFile(spotlight);
 });
 app.get("/json", function(req, res) {
+  if (process.env.MESSAGE_STYLE == uppercase) {
+    defMessage.message = defMessage.message.toUpperCase();
+  }
   res.json({"message": "Hello json"});
 });
 
