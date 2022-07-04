f1dceb6c7e5adfa5203fd98952025525e21152e5
Jim Lam <jimlam@146-109-254-113-on-nets.com>
2022-07-04 18:24:59 +0800

    here comes env

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 let spotlight = __dirname + "/views/index.html";
 let public = __dirname + "/public";
 console.log("Hello World", envConfig);
-let defMessage = {"message": "Hello json"};
+let defMessage = "Hello json"
 app.use(
   "/public",
   express.static(public)
@@ -14,9 +14,9 @@ app.get('/', function(req, res) {
 });
 app.get("/json", function(req, res) {
   if (process.env.MESSAGE_STYLE == "uppercase") {
-    defMessage.message = defMessage.message.toUpperCase();
+    defMessage = defMessage.toUpperCase();
   }
-  res.json(defMessage);
+  res.json({"message": defMessage});
 });
 
 
